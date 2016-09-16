d25b2aed65d39e1f67cd9ac2146d14c699e5ebc5
yangzhen19950126 <925721864@qq.com>
2016-09-16 20:18:48 +0800

    Update IUserService.java

--- src/com/briup/cms/service/IUserService.java
+++ src/com/briup/cms/service/IUserService.java
@@ -1,16 +1,18 @@
-package com.briup.cms.service;
+package com.mall.cn.service;
 
 import java.util.List;
 
-import com.briup.cms.bean.User;
+import com.mall.cn.bean.User;
 
 public interface IUserService {
-	//添加用户
+	// 添加用户
 	void add(User user);
-	//列出所有用户
+	// 列出所有用户
 	List<User> list();
-	//查询当前用户
+	// 查询当前用户
 	User find(User user);
-	//删除用户
+	// 删除用户
 	void delete(long id);
+	// 修改用户
+	public void update(User user);
 }
