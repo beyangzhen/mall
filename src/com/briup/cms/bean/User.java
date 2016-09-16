df23b17da33a6fd01ea6a0d6dc645f929e8556f0
yangzhen19950126 <925721864@qq.com>
2016-09-16 20:12:09 +0800

    Update User.java

--- src/com/briup/cms/bean/User.java
+++ src/com/briup/cms/bean/User.java
@@ -1,4 +1,4 @@
-package com.briup.cms.bean;
+package com.mall.cn.bean;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,9 +12,12 @@ import javax.persistence.Table;
 @Table(name="t_user")
 public class User {
 	private Long id;
-	private String username;
-	private	String password;
-	private String level; //M为管理员，C为顾客
+	private String username; // 用户名
+	private	String password; // 密码
+	private String level;    // 用户级别（M为管理员，C为顾客）
+	private String phone; 	 // 联系电话
+	private String address;  // 地址
+	private double account;  // 账户金额
 	
 	@Id
 	@GeneratedValue
@@ -36,12 +39,29 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
 	public String getLevel() {
 		return level;
 	}
 	public void setLevel(String level) {
 		this.level = level;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public double getAccount() {
+		return account;
+	}
+	public void setAccount(double account) {
+		this.account = account;
 	}
 	
 }
