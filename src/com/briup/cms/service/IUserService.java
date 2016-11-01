129cc012b937ae0082483e64aa8decfca28bdc24
beyangzhen <925721864@qq.com>
2016-11-01 20:16:03 +0800

    Update IUserService.java

--- src/com/briup/cms/service/IUserService.java
+++ src/com/briup/cms/service/IUserService.java
@@ -10,7 +10,9 @@ public interface IUserService {
 	// 列出所有用户
 	List<User> list();
 	// 查询当前用户
-	User find(User user);
+	User findByUser(User user);
+	// 根据id查询当前用户
+	User findById(long id);
 	// 删除用户
 	void delete(long id);
 	// 修改用户
