01c02c5efc225b88ccdb97c726841cb7f2ba127b
beyangzhen <925721864@qq.com>
2017-02-16 11:18:06 +0800

    Update UserServiceImpl.java

--- src/com/briup/cms/service/impl/UserServiceImpl.java
+++ src/com/briup/cms/service/impl/UserServiceImpl.java
@@ -9,9 +9,10 @@ import com.mall.cn.common.util.HibernateSessionFactory;
 import com.mall.cn.dao.UserDao;
 import com.mall.cn.service.IUserService;
 
+
 public class UserServiceImpl implements IUserService {
 	UserDao userDao;
-	
+
 	public UserServiceImpl() {
 		userDao = new UserDao();
 	}
@@ -20,11 +21,11 @@ public class UserServiceImpl implements IUserService {
 	public void add(User user) {
 		Session session = HibernateSessionFactory.getSession();
 		session.beginTransaction();
-		
+
 		userDao.save(user);
-		
-		//Assert.assertTrue(user.getId > 0); //junit的函数，测试是否满足条件
-		
+
+		// Assert.assertTrue(user.getId > 0); //junit的函数，测试是否满足条件
+
 		session.getTransaction().commit();
 		session.close();
 	}
@@ -33,38 +34,38 @@ public class UserServiceImpl implements IUserService {
 	public List<User> list() {
 		Session session = HibernateSessionFactory.getSession();
 		session.beginTransaction();
-		
+
 		List<User> list = userDao.findAll();
-		
+
 		session.getTransaction().commit();
-		session.close(); //可能包延迟加载异常
-		
+		session.close(); // 可能包延迟加载异常
+
 		return list;
 	}
-	
+
 	@Override
 	public User findByUser(User user) {
 		Session session = HibernateSessionFactory.getSession();
 		session.beginTransaction();
-		
+
 		User user2 = userDao.query(user);
-		
+
 		session.getTransaction().commit();
-		session.close(); //可能包延迟加载异常
-		
+		session.close(); // 可能包延迟加载异常
+
 		return user2;
 	}
-	
+
 	@Override
 	public User findById(long id) {
 		Session session = HibernateSessionFactory.getSession();
 		session.beginTransaction();
-		
+
 		User user = userDao.findById(id);
-		
+
 		session.getTransaction().commit();
-		session.close(); //可能包延迟加载异常
-		
+		session.close(); // 可能包延迟加载异常
+
 		return user;
 	}
 
@@ -72,22 +73,33 @@ public class UserServiceImpl implements IUserService {
 	public void delete(long id) {
 		Session session = HibernateSessionFactory.getSession();
 		session.beginTransaction();
-		
+
 		userDao.deleteById(id);
-		
+
 		session.getTransaction().commit();
-		session.close(); //可能包延迟加载异常
+		session.close(); // 可能包延迟加载异常
 	}
-	
+
 	@Override
 	public void update(User user) {
 		Session session = HibernateSessionFactory.getSession();
 		session.beginTransaction();
-		
+
 		userDao.update(user);
-		
+
 		session.getTransaction().commit();
-		session.close(); //可能包延迟加载异常
+		session.close(); // 可能包延迟加载异常
+	}
+
+	@Override
+	public void recharge(User user) {
+		Session session = HibernateSessionFactory.getSession();
+		session.beginTransaction();
+
+		userDao.update(user);
+
+		session.getTransaction().commit();
+		session.close(); // 可能包延迟加载异常
 	}
 
 }
