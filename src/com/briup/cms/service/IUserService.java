e359027bb75e13189d4ad6f30293709e0a91ebf9
beyangzhen <925721864@qq.com>
2017-02-16 11:17:31 +0800

    Update IUserService.java

--- src/com/briup/cms/service/IUserService.java
+++ src/com/briup/cms/service/IUserService.java
@@ -4,17 +4,26 @@ import java.util.List;
 
 import com.mall.cn.bean.User;
 
+
 public interface IUserService {
 	// 添加用户
 	void add(User user);
+
 	// 列出所有用户
 	List<User> list();
+
 	// 查询当前用户
 	User findByUser(User user);
+
 	// 根据id查询当前用户
 	User findById(long id);
+
 	// 删除用户
 	void delete(long id);
+
 	// 修改用户
 	public void update(User user);
+
+	// 账户充值
+	public void recharge(User user);
 }
