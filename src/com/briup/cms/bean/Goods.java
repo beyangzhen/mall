cb4d0f90acac7568ad6db743e0d39ff97326204a
beyangzhen <925721864@qq.com>
2017-02-21 16:21:49 +0800

    Update Goods.java

--- src/com/briup/cms/bean/Goods.java
+++ src/com/briup/cms/bean/Goods.java
@@ -7,67 +7,87 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+
 /**
  * 商品类
- * */
+ */
 @Entity
-@Table(name="t_goods")
+@Table(name = "t_goods")
 public class Goods {
 	private Long id;
 	private String name;  // 名称
-	private Double price; // 价格 
+	private Double price; // 价格
 	private String size;  // 大小
 	private int amount;   // 数量
 	private String type;  // 商品类型
-	
-	//多对一
+
+	// 多对一
 	private Category category;
-	
+
 	@Id
 	@GeneratedValue
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 	public String getSize() {
 		return size;
 	}
+
 	public void setSize(String size) {
 		this.size = size;
 	}
+
 	public int getAmount() {
 		return amount;
 	}
+
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	@ManyToOne
-	@JoinColumn(name="c_id")
+	@JoinColumn(name = "c_id")
 	public Category getCategory() {
 		return category;
 	}
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Goods [id=" + id + ", name=" + name + ", price=" + price + ", size=" + size + ", amount=" + amount
+				+ ", type=" + type + ", category=" + category + "]";
+	}
+
 }
